1c0feb3fa018a76dac27f7865f12226c7b06374c
Gautam <gautamthegreat21@gmail.com>
2021-12-08 22:54:18 +0530

    added multi file support

--- main.c
+++ main.c
@@ -1,17 +1,19 @@
 #include<stdio.h>
-#include<unistd.h>
 
 int main(int argc,char *argv[]){
 	char c;
 	int word_count = 0;
 
-	FILE *input = fopen(argv[1],"r");
-	
-	while((c = fgetc(input)) != EOF){
-		printf("%c",c);
-		word_count++;
-	}
+	for(int i = 1;i < argc ; i++){
 
+		FILE *input = fopen(argv[i],"r");
+
+
+		while((c = fgetc(input)) != EOF){
+			printf("%c",c);
+			word_count++;
+	}
+	}
 
 	printf("\nwords = %d\n",word_count - 1);
 }
