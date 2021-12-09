1a492cb0066e16c462888572b4b5e1afc9f8085a
Gautam <gautamthegreat21@gmail.com>
2021-12-09 14:18:46 +0530

    Added -c (clear) flag

--- main.c
+++ main.c
@@ -1,15 +1,30 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<string.h>
+
+int clear_buffer(char *flag, int *argc,int *last_arg);
 
 int main(int argc,char *argv[]){
 	char c;
-	int word_count = 0;
-	int new_lines = 0;
+	unsigned int word_count = 0;
+	unsigned int new_lines = 0;
+	unsigned int num_of_letters = 0;
+	int last_arg = argc - 1;
+	char *last_arg_val;
 
-	for(int i = 1;i < argc ; i++){
+	const char *cmd = "clear";
+
+	last_arg_val = argv[last_arg];
+	int use = clear_buffer(last_arg_val,&argc,&last_arg);
+
+	
+	for(int i = 1;i < argc ; i++){				
+		if(use == 0){
+			system(cmd);
+		}
 
 		FILE *input = fopen(argv[i],"r");
-
+			
 
 		while((c = fgetc(input)) != EOF){
 			printf("%c",c);
@@ -20,6 +35,17 @@ int main(int argc,char *argv[]){
 				new_lines ++;
 		}
 	}
+	
+	num_of_letters = word_count - 1 - (new_lines-1);
+	
+	printf("\nletters = %d\n",num_of_letters);
+}
 
-	printf("\nwords = %d\n",word_count - 1 - (new_lines - 1 ));
+int clear_buffer(char *flag, int *argc,int *last_arg){
+	if(strcmp(flag,"-c") == 0){
+		*argc = *last_arg;
+		return 0;
+	}
+	return 1;
+
 }
