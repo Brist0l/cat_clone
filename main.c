f52967bdef98f0cc83bc045be85b613b48bcd228
Gautam <gautamthegreat21@gmail.com>
2021-12-09 09:14:12 +0530

    Fixed the word count

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
+#include<string.h>
 
 int main(int argc,char *argv[]){
 	char c;
 	int word_count = 0;
+	int new_lines = 0;
 
 	for(int i = 1;i < argc ; i++){
 
@@ -11,9 +13,13 @@ int main(int argc,char *argv[]){
 
 		while((c = fgetc(input)) != EOF){
 			printf("%c",c);
+			
 			word_count++;
-	}
+			
+			if(c == '\n')
+				new_lines ++;
+		}
 	}
 
-	printf("\nwords = %d\n",word_count - 1);
+	printf("\nwords = %d\n",word_count - 1 - (new_lines - 1 ));
 }
